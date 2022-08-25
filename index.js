a32e6649c1070e002e02b66932ec8d742b71c28d
sean-code <john.nganga@student.moringaschool.com>
2022-08-25 18:30:56 +0300

    Lab Completed

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ function fetchBooks() {
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
+    const h3 = document.createElement('h3');
+    h3.textContent = book.name;
+    main.appendChild(h3);
   });
 }
 
